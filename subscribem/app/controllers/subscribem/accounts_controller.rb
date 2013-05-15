b5daa00148c9c17096b3abeafd29275b14cc4fe3
Ryan Bigg <radarlistener@gmail.com>
2013-05-16 09:15:53 +1000

    End of 'Building Subdomain Authentication' section

--- subscribem/app/controllers/subscribem/accounts_controller.rb
+++ subscribem/app/controllers/subscribem/accounts_controller.rb
@@ -8,8 +8,10 @@ module Subscribem
     end
 
     def create
-      @account = Subscribem::Account.new(params[:account])
-      if @account.save
+      @account = Subscribem::Account.create_with_owner(params[:account])
+      if @account.valid?
+        env['warden'].set_user(@account.owner.id, :scope => :user)
+        env['warden'].set_user(@account.id, :scope => :account)
         flash[:success] = "Your account has been successfully created."
         redirect_to subscribem.root_url(:subdomain => @account.subdomain)
       else
--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -4,5 +4,16 @@ module Subscribem
     accepts_nested_attributes_for :owner
     attr_accessible :name, :owner_attributes, :subdomain
     validates :subdomain, :presence => true, :uniqueness => true
+
+    has_many :members, :class_name => "Subscribem::Member"
+    has_many :users, :through => :members
+
+    def self.create_with_owner(params={})
+      account = new(params)
+      if account.save
+        account.users << account.owner
+      end
+      account
+    end
   end
 end
